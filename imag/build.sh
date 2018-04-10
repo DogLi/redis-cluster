49326d601a09e59a0564dab2af3b045c4432650e
linfeng.yuan <cxwcylf@126.com>
2018-04-11 02:07:50 +0800

    master ok

--- imag/build.sh
+++ imag/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t 127.0.0.1:30001/ylf/redis:0.1 .
 docker push 127.0.0.1:30001/ylf/redis:0.1
+#docker images | grep redis | awk '{print $3}' | xargs docker rmi
--- imag/run.sh
+++ imag/run.sh
@@ -19,24 +19,24 @@ function launchmaster() {
     echo "Redis master data doesn't exist, data won't be persistent!"
     mkdir /redis-master-data
   fi
-  sed -i "s/%redis-pass%/${REDIS_PASS}/" /redis-master/redis.conf
+  awk -v my_var=$REDISPASS '{ sub(/%redis-pass%/, my_var); print }' /redis-master/redis.tmp > /redis-master/redis.conf
   if [ $? -ne 0 ]; then
     echo "set redis password failed!"
     exit 1
   fi
-  redis-server /redis-master/redis.conf --protected-mode no
+  redis-server /redis-master/redis.conf
 }
 
 function launchsentinel() {
   while true; do
-    master=$(redis-cli -a $REDIS_PASS -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+    master=$(redis-cli -a $REDISPASS -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
     if [[ -n ${master} ]]; then
       master="${master//\"}"
     else
       master=${REDIS_MASTER_SERVICE_HOST}
     fi
 
-    redis-cli -a $REDIS_PASS -h ${master} INFO
+    redis-cli -a $REDISPASS -h ${master} INFO
     if [[ "$?" == "0" ]]; then
       break
     fi
@@ -47,7 +47,7 @@ function launchsentinel() {
   sentinel_conf=sentinel.conf
 
   echo "sentinel monitor mymaster ${master} 6379 2" > ${sentinel_conf}
-  echo "sentinel auth-pass mymaster ${REDIS_PASS}" >> ${sentinel_conf}
+  echo "sentinel auth-pass mymaster ${REDISPASS}" >> ${sentinel_conf}
   echo "sentinel down-after-milliseconds mymaster 60000" >> ${sentinel_conf}
   echo "sentinel failover-timeout mymaster 180000" >> ${sentinel_conf}
   echo "sentinel parallel-syncs mymaster 1" >> ${sentinel_conf}
@@ -58,7 +58,7 @@ function launchsentinel() {
 
 function launchslave() {
   while true; do
-    master=$(redis-cli -a $REDIS_PASS -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+    master=$(redis-cli -a $REDISPASS -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
     if [[ -n ${master} ]]; then
       master="${master//\"}"
     else
@@ -66,16 +66,15 @@ function launchslave() {
       sleep 60
       exit 1
     fi 
-    redis-cli -a $REDIS_PASS -h ${master} INFO
+    redis-cli -a $REDISPASS -h ${master} INFO
     if [[ "$?" == "0" ]]; then
       break
     fi
     echo "Connecting to master failed.  Waiting..."
     sleep 10
   done
-  sed -i "s/%master-ip%/${master}/" /redis-slave/redis.conf
-  sed -i "s/%master-port%/6379/" /redis-slave/redis.conf
-  sed -i "s/%redis-pass%/${REDIS_PASS}/" /redis-slave/redis.conf
+  awk -v my_var=$master '{ sub(/%master-ip%/, my_var); print }' /redis-slave/redis.conf > /redis-slave/redis.conf
+  awk -v my_var=$REDISPASS '{ sub(/%redis-pass%/, my_var); print }' /redis-slave/redis.conf > /redis-slave/redis.conf
   redis-server /redis-slave/redis.conf --protected-mode no
 }
 
